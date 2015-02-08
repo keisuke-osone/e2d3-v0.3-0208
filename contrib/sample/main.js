fea1bcd370550b6233076d7682040661ec0a1d2a
ishikake <ishikake@gmail.com>
2015-02-08 17:31:22 +0900

    test

--- contrib/sample/main.js
+++ contrib/sample/main.js
@@ -26,7 +26,6 @@ drawArea = d3.select('#e2d3-chart-area').append('svg')
     .attr('transform', 'translate(' + leftMargin + ',' + topMargin + ')')
 
 
-
 //Excelにデータがセットされた後、最初に呼ばれるメソッド（必須）
 function e2d3Show(isUpdate) {
 
@@ -48,7 +47,6 @@ function e2d3Update(responce) {
 }
 
 
-var metrics = 'wind_speed';
 
 //変換されたjsonデータを使ってグラフ描画
 function show(data) {
@@ -56,10 +54,29 @@ function show(data) {
     //dataは、bind2jsonで渡すdimensionオプションによって、整形されたJsonオブジェクトです。
     //描画は、#e2d3-chart-area 内にしてください。
         
+    // UIからdata1とdata2の列名を取得する。
+    
+    // for debug
+    //var currentData1Name = "sunshine_duration"
+    //var currentData2Name = "wind_speed"
+    var currentData1Name = "Fish_shellfish "
+    var currentData2Name = "Meals_outside"
+    var currentData3Name = "Electricity"
+    
+    xMetrics = currentData1Name;
+    yMetrics = currentData2Name;
+    zMetrics = currentData3Name;
+
+    
+    // UIからスライダーの位置を取得する。
+    var currentIndex = 20;
+    
+        
+        
     // 最大値の取得
     var xMax = d3.max(data, function (d) { return parseInt(d[xMetrics], 10) + 1})
     // 最小値の取得
-    var xMin = d3.min(data, function (d) { return d[xMetrics]})
+    var xMin = d3.min(data, function (d) { return parseInt(d[xMetrics], 10)})
 
     // 最大値の取得
     var yMax = d3.max(data, function (d) { return parseInt(d[yMetrics], 10) + 1})
@@ -70,6 +87,8 @@ function show(data) {
     var zMax = d3.max(data, function (d) { return parseInt(d[zMetrics], 10) + 1})
     // 最小値の取得
     var zMin = d3.min(data, function (d) { return d[zMetrics]})
+
+    
 
     // xのスケールの設定
     var xScale = d3.scale.linear()
@@ -108,12 +127,62 @@ function show(data) {
     drawArea
         .append('g')
         .attr('class', 'y axis')
-        .call(yAxis)
+        .call(yAxis);
+        
+        
         
     // 散布図の描画
     drawArea
         .selectAll('circle')
         .data(data)
+        .enter()
+        .append('circle')
+        .on('click', function (d) {
+            alert(yMetrics + d[yMetrics])
+        })
+        //.on('mouseover', function (d) {
+        //    d3.select(this)
+        //        .attr('fill', 'orange')
+        //})
+        //.on('mouseout', function (d) {
+        //    d3.select(this)
+        //        .attr('fill', 'red');
+        //})
+        .attr('fill', '#f00')
+        .attr('r', function (d, i) {
+            if (i === currentIndex) {
+                d3.select(this).attr('fill', 'orange'); 
+                return 10;
+            } else {
+                return 5;
+            }
+        })
+        .attr('cx', function (d) {
+            return xScale(d[xMetrics]);
+        })        
+        .attr('cy', function (d) {
+            return yScale(d[yMetrics])
+        })
+        .transition()
+        .duration(1000)
+        .delay(function(d, i) {
+            return  i * 20;
+        })
+        .ease('bounce')
+        //.attr('r', function(d, i) {
+        //    return zScale(d[zMetrics])
+        //});
+        
+    
+
+    // 選択された行のデータを描画
+    /*
+    drawArea
+        .selectAll('circle')
+        .data([{
+            'x': data[1][5],
+            'y': data[2][5]
+            }])
         .enter()
         .append('circle')
         .on('click', function (d) {
@@ -130,10 +199,10 @@ function show(data) {
         .attr('fill', '#f00')
         .attr('r', 0)
         .attr('cx', function (d) {
-            return xScale(d[xMetrics]);
+            return xScale(x);
         })        
         .attr('cy', function (d) {
-            return yScale(d[yMetrics])
+            return yScale(y)
         })
         .transition()
         .duration(1000)
@@ -144,6 +213,9 @@ function show(data) {
         .attr('r', function(d, i) {
             return zScale(d[zMetrics])
         });
+        */
+
+        
 }
 
 
